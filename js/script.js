836c721800b772093a3b0712e7111942d044f005
M1yamotoo0 <89333992+M1yamotoo0@users.noreply.github.com>
2022-04-03 14:19:19 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+  // Кнопка наверх
+
   let btn = $('.scroll-up-link');
   $(window).scroll(function () {
     if ($(window).scrollTop() > 300) {
@@ -13,6 +15,7 @@ $(document).ready(function () {
     $('html, body').animate({ scrollTop: 0 }, '300');
   });
 
+  // Табы
 
   $('ul.main__list--bg').on('click', 'li:not(.main__item--bg--active)', function () {
     $(this)
@@ -20,9 +23,21 @@ $(document).ready(function () {
       .closest('div.main-content__tabs').find('div.main-content__tab-content').removeClass('main-content__tab-content--active').eq($(this).index()).addClass('main-content__tab-content--active');
   });
 
-  $(".accordeon dd").hide().prev().click(function() {
+  // Аккордеон
+
+  $(".accordeon dd").hide().prev().click(function () {
     $(this).parents(".accordeon").find("dd").not(this).slideUp().prev().removeClass("active");
     $(this).next().not(":visible").slideDown().prev().addClass("active");
   });
+
+  // Открытие элементов аккордеона с других страниц
+
+  if (window.location.hash) {
+    var $hash = window.location.hash,
+      $hash_text = $hash.substr(1);
+    $('dt').next().hide();
+    $('dt[data-name="' + $hash_text + '"]').toggleClass('active');
+    $('dt.active').next().show();
+  }
 
 });
